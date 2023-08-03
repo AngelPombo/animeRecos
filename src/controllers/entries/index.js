689aac0d069293a0e42663fa08acf0abf74461b0
Angel <angelpombo.92@gmail.com>
2023-08-03 12:54:48 +0200

    PostEntry y ya bastante fue(trabajo en equipo)

--- src/controllers/entries/index.js
+++ src/controllers/entries/index.js
@@ -3,6 +3,7 @@ const getOneEntry = require("./getOneEntry");
 const getEntriesByCategory = require("./getEntriesByCategory");
 const getEntriesByGenre = require("./getEntriesByGenre");
 const getTopRatedEntriesByCategory = require("./getTopRatedEntriesByCategory");
+const postEntry = require("./postEntry");
 
 
 module.exports = {
@@ -10,5 +11,6 @@ module.exports = {
     getOneEntry,
     getEntriesByCategory,
     getEntriesByGenre,
-    getTopRatedEntriesByCategory
+    getTopRatedEntriesByCategory,
+    postEntry
 }
