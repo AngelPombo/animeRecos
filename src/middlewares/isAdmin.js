27f142eaaa3fa38e07435eefd814b72633495d44
Antia Varela <anmavarpe@outlook.es>
2023-08-30 21:31:37 +0200

    bugfix: no había guardado un par de archivos (jeje). REVISAR, NO COMPROBADO

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -6,7 +6,7 @@ async function isAdmin (req,res,next){
         if(role !== 'admin'){
             return res.status(401).send('No puedes acceder a este contenido si no eres un administrador');
         }
-
+        
     }catch(e){
         console.log(e);
     }finally{
