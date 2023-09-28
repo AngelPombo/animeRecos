183e71efe3b27c98bc1408e3e024f1576b96e0b3
Antia Varela <anmavarpe@outlook.es>
2023-09-29 00:13:48 +0200

    bufgix: comprobación de exp de token, por si cierras el navegador sin deslogearte y se quedan guardados datos en el localStorage --- trabajo conjunto

--- client/src/context/sessionContext.jsx
+++ client/src/context/sessionContext.jsx
@@ -12,20 +12,34 @@ function SessionProvider(props) {
   const [logged, setLogged] = useState(false);
   const [username, setUsername] = useState("anónimo");
   const [userId, setUserId] = useState("");
+  const [timeStamp, setTimeStamp] = useState(0);
+  const oneDayMs = 86400000;
 
   useEffect(() => {
     const token = window.localStorage.getItem("jwt");
-    if (token) {
+    if (token && (new Date().getTime() < (timeStamp + oneDayMs))) {
       setLogged(true);
       setUsername(window.localStorage.getItem("email"));
       setUserId(window.localStorage.getItem("id"));
+      setTimeStamp(window.localStorage.getItem("timestamp"))
+    }else{
+      setTimeStamp(0);
+      setLogged(false);
+      setUsername("Anónimo");
+      setUserId("");
+      window.localStorage.removeItem("jwt");
+      window.localStorage.removeItem("email");
+      window.localStorage.removeItem("id");
+      window.localStorage.removeItem("timestamp");
     }
   }, []);
 
-  function handleLogin({ auth, email, token, id }) {
+  function handleLogin({ auth, email, token, id, timestamp }) {
     window.localStorage.setItem("jwt", token);
     window.localStorage.setItem("email", email);
     window.localStorage.setItem("id", id);
+    window.localStorage.setItem("timestamp", timestamp);
+    setTimeStamp(timestamp);
     setLogged(auth);
     setUsername(email);
     setUserId(id);
@@ -35,13 +49,15 @@ function SessionProvider(props) {
     window.localStorage.removeItem("jwt");
     window.localStorage.removeItem("email");
     window.localStorage.removeItem("id");
+    window.localStorage.removeItem("timestamp");
+    setTimeStamp(null);
     setLogged(false);
     setUsername("Anónimo");
     setUserId("");
   }
 
   return (
-    <sessionContext.Provider value={{ logged, handleLogin, handleLogout, username, userId }}>
+    <sessionContext.Provider value={{ logged, handleLogin, handleLogout, username, userId, timeStamp, oneDayMs}}>
       {props.children}
     </sessionContext.Provider>
   );
